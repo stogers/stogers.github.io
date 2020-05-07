7f968efc570a6c9ea8bd95d051ca1aa306921f12
Stephanie Rogers <stephanierogers@Stephanies-MacBook-Pro.local>
2020-05-06 22:52:20 -0700

    change aiw title

--- js/units.js
+++ js/units.js
@@ -8,7 +8,7 @@ var Units = {
             'chapters': [pp1,pp2,pp3,pp4,pp5,pp6,pp7,pp8,pp9,pp10,pp11,pp12,pp13,pp14,pp15,pp16,pp17,pp18,pp19,pp20,pp21,pp22,pp23,pp24,pp25,pp26,pp27,pp28,pp29,pp30,pp31,pp32,pp33,pp34,pp35,pp36,pp37,pp38,pp39,pp40,pp41,pp42,pp43,pp44,pp45,pp46,pp47,pp48,pp49,pp50,pp51,pp52,pp53,pp54,pp55,pp56,pp57,pp58,pp59,pp60,pp61],
         },
         2 : {
-            'title': 'Alice\'s Adventures in Wonderland with a very long title',
+            'title': 'Alice\'s Adventures in Wonderland',
             'author': 'Lewis Carroll',
             'chapters': [aiw1, aiw2, aiw3, aiw4, aiw5, aiw6, aiw7, aiw8, aiw9, aiw10, aiw11, aiw12],
             'chapterNames': ["Chapter 1: Down the Rabbit-Hole", "Chapter 2: The Pool of Tears", "Chapter 3: A Caucus-Race and a Long Tale", "Chapter 4: The Rabbit Sends in a Little Bill", "Chapter 5: Advice from a Caterpillar", "Chapter 6: Pig and Pepper", "Chapter 7: A Mad Tea-Party", "Chapter 8: The Queen’s Croquet-Ground", "Chapter 9: The Mock Turtle’s Story", "Chapter 10: The Lobster Quadrille", "Chapter 11: Who Stole the Tarts?", "Chapter 12: Alice’s Evidence"]
